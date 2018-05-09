1275bd447bd01656c8815cb39b4eec5db03ac51e
Raquel <rpezoarivera@gmail.com>
2018-05-09 13:43:37 -0300

    adding under_sampling parameter

--- scripts/run_classification.sh
+++ scripts/run_classification.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-seeds="0 1 2 3 4 5 6 7 8 9"
+seeds="0" #1 2 3 4 5 6 7 8 9"
 
 #im=2+_8
 #feat="2+_8_rpr"
@@ -10,6 +10,7 @@ im=${1}
 feat=${2}
 type_im=${3}
 clf=${4}
+under_sampling=${5}
 
 data_dir=/home/rpezoa/experiment_data/
 #out_dir=output/${feat}_halton_patches/
@@ -19,10 +20,9 @@ bt_path=${data_dir}big_${type_im}/labels/${im}.npy
 f_path=${data_dir}${out_dir}feat_vectors/
 t_path=${data_dir}${out_dir}target_vectors/
 
-echo ${data_dir}${sub_dir_out}
 for s in $seeds;do
 
-python3 classification.py ${data_dir}/big_${type_im}/ ${data_dir}${out_dir}${clf} ${im}.tif  --seed ${s}  -is 1000 -bp ${b_path}  -btp ${bt_path} -tp ${t_path}${s}.npy -feat_path ${f_path}${s}.npy -clf ${clf}
+python3 classification.py ${data_dir}/big_${type_im}/ ${data_dir}${out_dir}${clf}_${under_sampling} ${im}.tif  --seed ${s}  -is 1000 -bp ${b_path}  -btp ${bt_path} -tp ${t_path}${s}.npy -feat_path ${f_path}${s}.npy -clf ${clf} -us ${under_sampling}
 
 done
 
