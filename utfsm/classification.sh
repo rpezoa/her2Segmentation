58d08bbd7cb0934bb9afd7fc1274067814f25e75
Raquel Pezoa <rpezoarivera@gmail.com>
2018-06-04 09:06:34 -0400

    new runs on utfsm

--- utfsm/classification.sh
+++ utfsm/classification.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q gpuk
 #PBS -N log_${clf}_${seed}
-#PBS -l walltime=3:00:00
+#PBS -l walltime=12:00:00
 
 cd $PBS_O_WORKDIR
 pwd
@@ -16,7 +16,9 @@ s=${seed}
 cluster=${cluster}
 local_rw=${rw}
 
-data_dir=/data/user/r/rpezoa/experiment_data/
+
+#data_dir=/data/user/r/rpezoa/experiment_data/
+data_dir=/data/tsunami/user/r/rpezoa/experiment_data/
 out_dir=output/${im}_${feat}_${type_training_data}/
 b_path=${data_dir}big_${type_im}/features/${im}_rpr.npy
 bt_path=${data_dir}big_${type_im}/labels/${im}.npy
@@ -26,7 +28,7 @@ t_path=${data_dir}${out_dir}target_vectors/
 use anaconda3
 gcc48
 
-python ../scripts/classification.py ${data_dir}/big_${type_im}/ ${data_dir}${out_dir}${clf}_${under_sampling} ${im}.tif  --seed ${s}  -is 1000 -bp ${b_path}  -btp ${bt_path} -tp ${t_path}${s}.npy -feat_path ${f_path}${s}.npy -clf ${clf} -us ${under_sampling} --cluster ${cluster} --local_rw ${local_rw}
+python ../scripts/classification.py ${data_dir}/big_${type_im}/ ${data_dir}${out_dir}${clf}_${under_sampling}_cross ${im}.tif  --seed ${s}  -is 1000 -bp ${b_path}  -btp ${bt_path} -tp ${t_path}${s}.npy -feat_path ${f_path}${s}.npy -clf ${clf} -us ${under_sampling} --cluster ${cluster} --local_rw ${local_rw}
 
 
 
--- utfsm/qsub_cluster.sh
+++ utfsm/qsub_cluster.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-#qsub -v im="2+_8",type_im="2+",clf="deep",training="halton_patches",feat="rpr",under_sampling="0",seed="0",cluster="1" classification.sh
-qsub -v im="2+_8",type_im="2+",clf="deep",training="halton_patches",feat="rpr",under_sampling="0",seed="5",cluster="1",rw="2" classification.sh
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="halton_patches",feat="rpr",under_sampling="0",seed="3",cluster="1" classification.sh
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="halton_patches",feat="rpr",under_sampling="0",seed="4",cluster="1" classification.sh
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="halton_patches",feat="rpr",under_sampling="0",seed="5",cluster="1" classification.sh
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="halton_patches",feat="rpr",under_sampling="0",seed="6",cluster="1" classification.sh
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="halton_patches",feat="rpr",under_sampling="0",seed="7",cluster="1" classification.sh
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="halton_patches",feat="rpr",under_sampling="0",seed="8",cluster="1" classification.sh
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="halton_patches",feat="rpr",under_sampling="0",seed="9",cluster="1" classification.sh
 
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="random_patches",feat="rpr",under_sampling="0",seed="8",cluster="1" classification.sh
-#qsub -v im="2+_8",type_im="2+",clf="svm",training="random_patches",feat="rpr",under_sampling="0",seed="9",cluster="1" classification.sh
+images=("1+_20" "1+_25" "2+_1" "2+_8" "2+_9" "3+_19")
+type_im=("1+" "1+" "2+" "2+" "2+" "3+")
+seeds="0 1 2 3 4 5 6 7 8 9"
+methods="halton_patches" # random_patches"
+images=("1+_20")
+type_im=("1+")
+seeds="0"
+for m in $methods;do
+for s in $seeds;do
+	for i in `seq 0 0`;do
+		echo $m $s ${images[$i]} ${type_im[$i]}
+    		qsub -v im=${images[$i]},type_im=${type_im[$i]},clf="deep",training=${m},feat="rpr",under_sampling="0",seed=${s},cluster="1",rw="2" classification.sh
+		sleep 1
+	done;
+done;
+done;
+
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="0",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="1",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="2",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="3",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="4",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="5",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="6",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="7",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="8",cluster="1",rw="2" classification.sh
+#qsub -v im="1+_20",type_im="1+",clf="knn",training="random_patches",feat="rpr",under_sampling="0",seed="9",cluster="1",rw="2" classification.sh
+#
+
