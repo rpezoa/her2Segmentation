9cfdb7970f18bb8d4c27996abaf6ab4f06f916a6
Raquel Pezoa <rpezoarivera@gmail.com>
2018-06-04 09:30:54 -0400

    adding cross validation

--- scripts/classification.py
+++ scripts/classification.py
@@ -8,6 +8,7 @@ import time
 from data import under_sampling as under_s
 import argparse
 from shutil import copyfile
+from sklearn.externals import joblib
 
 import random as rd
 from sklearn.grid_search import GridSearchCV
@@ -22,6 +23,10 @@ from keras import metrics
 from keras.wrappers.scikit_learn import KerasClassifier
 
 from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.ensemble import ExtraTreesClassifier
 
 from config_A import *
 
@@ -118,6 +123,8 @@ if not os.path.exists(out_dir + "/cm"):
     os.makedirs(out_dir + "cm")
 if not os.path.exists(out_dir + "/big_prob"):
     os.makedirs(out_dir + "big_prob")
+if not os.path.exists(out_dir + "/params"):
+    os.makedirs(out_dir + "params")
 
 nX,ny= current_y.sum(), current_y.sum()
 print(ny)
@@ -139,6 +146,13 @@ print("current_X.mean()", current_X.mean())
 
 N = current_X.shape[0]
 
+def save_results(best_score, best_params,t,clf):
+    joblib.dump(best_score, out_dir  + "params/" + str(seed) + "_score.pkl")
+    joblib.dump(best_params, out_dir  + "params/" + str(seed) + "_params.pkl")
+    np.save(out_dir + "times/" + str(seed) + ".npy", t)
+    print("Best: %f using %s"% (best_score, best_params))
+    print(clf, "time:", t)
+
 def svm_c(X,y,big_X):
     print("SVM :::::: X,y", X.shape, y.shape) 
     tuned_parameters = [{'kernel': [kernel], 'gamma': gamma_range,'C': C_range}]
@@ -152,8 +166,6 @@ def svm_c(X,y,big_X):
     pred_big_im = best_estimator.predict(big_X)
     prob_big_im = best_estimator.predict_proba(big_X)
     return pred_vector, pred_big_im, prob_big_im
-
-
 
 def create_model():
     activation="relu"
@@ -174,10 +186,10 @@ def deep_l(X,y,big_X):
     ## https://datascience.stackexchange.com/questions/13746/how-to-define-a-custom-performance-metric-in-keras
     #clf.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
     t1 = time.time()
-    model = KerasClassifier(build_fn=create_model, batch_size=1000, epochs=10)
+    model = KerasClassifier(build_fn=create_model)
     
-    epochs = [1,10,100,1000]
-    batch_size = [10,100,1000,]
+    epochs = [1] #[10,100,1000]
+    batch_size = [5] #[10, 50, 100, 150]
     param_grid = dict(epochs=epochs, batch_size=batch_size)
     grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=5)
     grid_result = grid.fit(x_train, y_train)
@@ -187,34 +199,57 @@ def deep_l(X,y,big_X):
     #pred_big_im = clf.predict(big_scaled_feat_matrix)
     #pred_big_im = (pred_big_im > 0.5)
     t2 = time.time()
-    print("Deep Learning time:", t2-t1)
+    t = t2 - t1
+    save_results(grid_result.best_score_, grid_result.best_params_,t,"Deep")
     return pred_big_im, pred_big_im, pred_big_im
 
 def random_f(X,y,big_X):
-    from sklearn.ensemble import RandomForestClassifier
-    print("Random forest :::::: X,y", X.shape, y.shape) 
-    clf = RandomForestClassifier(n_estimators=25, criterion="entropy")
+    # https://towardsdatascience.com/hyperparameter-tuning-the-random-forest-in-python-using-scikit-learn-28d2aa77dd74
+    print("Random forest :::::: X,y", X.shape, y.shape)
     t1=time.time()
-    clf.fit(x_train, y_train)
+    param_grid = {'criterion': ['gini', 'entropy'],
+    'max_features': [2, 3,4],
+    'min_samples_leaf': [2,3,4],
+    'n_estimators': [5, 15, 25,35]}
+    clf = RandomForestClassifier(random_state=0)
+    grid_result = GridSearchCV(estimator = clf, param_grid = param_grid, cv = 5, n_jobs = -1, verbose = 2)
+    grid_result.fit(x_train, y_train)
+    pred_vector = grid_result.best_estimator_.predict(X)
+    pred_big_im = grid_result.best_estimator_.predict(big_X)
     t2=time.time()
-    pred_vector = clf.predict(X)
-    pred_big_im = clf.predict(big_X)
+    t = t2-t1
+    save_results(grid_result.best_score_, grid_result.best_params_,t,"RF")
     return pred_vector, pred_big_im, pred_big_im
 
 def kNN(X,y,big_X):
     print("KNN :::::: X,y", X.shape, y.shape) 
-    t1=time.time()
+    t1 = time.time()
     param_grid = {'n_neighbors': np.arange(20)+1, 'weights': ['uniform', 'distance']}
-
     knn = KNeighborsClassifier()
-    clf = GridSearchCV(knn,parameters,cv=5, scoring="accuracy")
+    clf = GridSearchCV(knn,param_grid,cv=5, scoring="f1")
     print("::::::: Training with ", x_train.shape, ":::::::::::::")
     grid_result = clf.fit(x_train, y_train)
-    print("SVM fitting time:", t2-t1)
+    pred_vector = grid_result.best_estimator_.predict(X)
+    pred_big_im = grid_result.best_estimator_.predict(big_X)
+    t2 = time.time()
+    t = t2 - t1
+    save_results(grid_result.best_score_, grid_result.best_params_,t,"KNN")
+    return pred_vector, pred_big_im, pred_big_im
+
+
+def extra_trees(X,y,big_X):
+    print("Extra Trees :::::: X,y", X.shape, y.shape)
+    t1 = time.time()
+    param_grid = {'n_estimators':[5,15,25,35],'class_weight':['balanced'], 'criterion':["gini","entropy"]} 
+    et = ExtraTreesClassifier(random_state=0)
+    clf = GridSearchCV(estimator = et, param_grid=param_grid,cv=5, n_jobs=-1, verbose=2) 
+    print("::::::: Training with ", x_train.shape, ":::::::::::::")
+    grid_result = clf.fit(x_train, y_train)
     pred_vector = grid_result.best_estimator_.predict(X)
     pred_big_im = grid_result.best_estimator_.predict(big_X)
     t2=time.time()
-    print("KNN time:", t2-t1)
+    t = t2 - t1
+    save_results(grid_result.best_score_, grid_result.best_params_,t,"ET")
     return pred_vector, pred_big_im, pred_big_im
 
 def xgboost(X,y,big_X):
@@ -232,20 +267,6 @@ def xgboost(X,y,big_X):
     # Generating predictions and saving the predictions
     pred_big_im = bst.predict(big_X)
     return pred_big_im, pred_big_im,pred_big_im
-
-def extra_trees(X,y,big_X):
-    from sklearn.ensemble import ExtraTreesClassifier
-    print("Extra Trees :::::: X,y", X.shape, y.shape) 
-    clf = ExtraTreesClassifier(n_estimators=25)
-    print("::::::: Training with ", x_train.shape, ":::::::::::::")
-    t1=time.time()
-    clf.fit(x_train, y_train)
-    t2=time.time()
-    print("SVM fitting time:", t2-t1)
-    pred_vector = clf.predict(X)
-    pred_big_im = clf.predict(big_X)
-    return pred_vector, pred_big_im, pred_big_im
-
 
 if c_n_mem_pixels > 3:
     x_train, x_test, y_train, y_test = train_test_split(current_X, current_y, test_size=0.2, random_state=0)
